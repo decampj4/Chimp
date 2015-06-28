9575414f83959b80f3c5f61e6a05968c5b6340ef
Justin DeCamp <decampj4@gmail.com>
2015-06-28 11:33:36 -0500

    SockJS in place - OpenSource library that allows the automatic upgrading of HTTP requests to the Websocket protocol

--- API/src/main/java/com/chimp/websocket/WebSocketConfig.java
+++ API/src/main/java/com/chimp/websocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(getWebSocketHandler(), "/webSocketHandler").withSockJS();
+        registry.addHandler(getWebSocketHandler(), "/websocket").withSockJS();
     }
 
     @Bean
--- WEBAPP/index.js
+++ WEBAPP/index.js
@@ -1,13 +1,12 @@
-var sock = new SockJS('http://localhost/chimp/webSocketHandler');
- sock.onopen = function() {
-     console.log('open');
- };
- sock.onmessage = function(e) {
-     console.log('message', e.data);
- };
- sock.onclose = function() {
-     console.log('close');
- };
+var sock = new SockJS('http://localhost:8080/chimp/websocket');
+sock.onopen = function () {
+   console.log('open');
+   sock.send('test');
+};
+sock.onmessage = function (e) {
+   console.log('message', e.data);
+};
+sock.onclose = function () {
+   console.log('close');
+};
 
- sock.send('test');
- sock.close();
